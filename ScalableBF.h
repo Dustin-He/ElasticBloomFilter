c4b4ccc3a116794d8a2ffbc7b7366c55ab81f693
Dustin-He <57241633+Dustin-He@users.noreply.github.com>
2020-04-17 20:10:26 +0800

    Add files via upload

---
+++ ScalableBF.h
@@ -0,0 +1,90 @@
+#ifndef _SBF_H
+#define _SBF_H
+
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<cstdlib>
+#include<cmath>
+#include "BOBHash32.h"
+#include "param.h"
+
+using namespace std;
+
+class BF
+{
+	public:
+	bool* c;
+	int num;
+	BF() {};
+	BF(int m)
+	{
+		num = 0;
+		c = new bool[1<<m];
+		memset(c, 0, 1<<m);
+	}
+};
+class ScalableBF
+{
+	public:
+	BOBHash32 * bobhash[HASH_NUM + 2];
+	int w, m;
+	double c;
+	BF* b[10100];
+	ScalableBF(){};
+	ScalableBF(int _m, double _c)
+	{
+		w = 0;
+		m = _m;
+		c = _c;
+		memset(b, 0, sizeof(b));
+		b[0] = new BF(m);
+		for (int i = 0; i < HASH_NUM; i++)
+			bobhash[i] = new BOBHash32(i + 1005);
+	}
+	void insert(int x)
+	{
+		if(b[w]->num >= c * (1<<(m+w)))
+		{
+			w++;
+			b[w] = new BF(m + w);
+			c* = 0.9;
+		}
+		for(int i = 0; i < HASH_NUM; i++)
+		{
+			int z = bobhash[i]->run((char *)&x, 4)&((1<<(m+w)) - 1);
+			if(!b[w]->c[z])
+			{
+				b[w]->num++;
+				b[w]->c[z] = 1;
+			}
+		}
+	}
+	bool query(int x)
+	{
+		int *z = new int[HASH_NUM];
+		for(int j = 0; j < HASH_NUM; j++)
+			z[j] = bobhash[j]->run((char *)&x,4)&((1<<(m+w))-1);
+		for(int i = 0; i <= w; i++)
+		{
+			bool f = 1;
+			for(int j = 0; j < HASH_NUM; j++)
+			{
+				if(!b[i]->c[z[j]&((1<<(m+i))-1)])
+				{
+					f=0;
+					break;
+				}
+			}
+			if(f)
+			{
+				delete []z;
+				return true;
+			}
+		}
+		delete []z;
+		return false;
+	}
+};
+
+#endif 
